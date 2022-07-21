ae45809a9efc9707a6fe156830a2339c52ebd500
Koenig, Jennifer <jennifer.koenig@sva.de>
2022-07-21 14:11:32 +0200

    First commit

---
+++ setup/azurestorage.tf
@@ -0,0 +1,67 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "setup" {
+  name     = local.az_resource_group_name
+  location = var.az_location
+}
+
+resource "azurerm_storage_account" "sa" {
+  name                     = local.az_storage_account_name
+  resource_group_name      = azurerm_resource_group.setup.name
+  location                 = var.az_location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  min_tls_version          = "TLS1_2"
+  enable_https_traffic_only = true
+  queue_properties  {
+    logging {
+        delete                = true
+        read                  = true
+        write                 = true
+        version               = "1.0"
+        retention_policy_days = 10
+    }
+  }
+}
+
+resource "azurerm_storage_container" "ct" {
+  name                 = "terraform-state"
+  storage_account_name = azurerm_storage_account.sa.name
+
+}
+
+data "azurerm_storage_account_sas" "state" {
+  connection_string = azurerm_storage_account.sa.primary_connection_string
+  https_only        = true
+
+  resource_types {
+    service   = true
+    container = true
+    object    = true
+  }
+
+  services {
+    blob  = true
+    queue = false
+    table = false
+    file  = false
+  }
+
+  start  = timestamp()
+  expiry = timeadd(timestamp(), "17520h")
+
+  permissions {
+    read    = true
+    write   = true
+    delete  = true
+    list    = true
+    add     = true
+    create  = true
+    update  = false
+    process = false
+    filter = false
+    tag = false
+  }
+}
